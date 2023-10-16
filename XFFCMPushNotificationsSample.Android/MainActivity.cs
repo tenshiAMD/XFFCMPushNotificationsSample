27540aeae5e95f5b72fc921ec63cf76e22b6dafc
Angel Aviel Domaoan <13580338+tenshiAMD@users.noreply.github.com>
2023-10-17 04:28:13 +0800

    Fix build issues

--- XFFCMPushNotificationsSample.Android/MainActivity.cs
+++ XFFCMPushNotificationsSample.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Plugin.FirebasePushNotification;
 
 namespace XFFCMPushNotificationsSample.Droid
 {
-    [Activity(Label = "XFFCMPushNotificationsSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "PushNotificationsSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
